4d973f62b47f28351a96b1b1a6e0a7d7cc35497d
Raul Blanco Vazquez <121907539+raau1@users.noreply.github.com>
2024-11-03 12:06:26 +0000

    Delete campus_locator directory
    Signed-off-by: Raul Blanco Vazquez <121907539+raau1@users.noreply.github.com>

--- campus_locator/poi_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PoiAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'poi_app'
--- campus_locator/poi_app/urls.py
+++
@@ -1,7 +0,0 @@
-# poi_app/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.poi_list, name='poi_list'),
-]
